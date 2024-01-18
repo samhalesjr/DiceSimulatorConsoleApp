acdb3140b64562f001c86dcec6272ce858ee66ca
samha <samha@SAM-DELL-XPS-13>
2024-01-17 18:06:31 -0700

    Add project files.

---
+++ DiceSimulatorConsoleApp/DiceSimulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DiceSimulatorConsoleApp
+{
+    public class DiceSimulator
+    {
+        private Random random = new Random();
+
+        public int[] SimulateRolls(int numberOfRolls)
+        {
+            int[] results = new int[11]; // For combinations 2-12
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int roll = random.Next(1, 7) + random.Next(1, 7);
+                results[roll - 2]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ DiceSimulatorConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DiceSimulatorConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int totalRolls = int.Parse(Console.ReadLine());
+
+            DiceSimulator simulator = new DiceSimulator();
+            int[] rollResults = simulator.SimulateRolls(totalRolls);
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {totalRolls}.\n");
+
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write($"{i}: ");
+                int percentage = rollResults[i - 2] * 100 / totalRolls;
+                Console.WriteLine(new String('*', percentage));
+            }
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
